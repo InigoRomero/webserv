57689b08d3ec75251ae5a3ab499c6b5dc1903af7
InigoRomero <igoromero@gmail.com>
2021-01-25 18:29:40 +0100

    clean code

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -21,15 +21,12 @@ int Server::start(void)
 		std::cout << "Error: " << "Server::start -> socket(): " << std::string(strerror(errno)) << std::endl;
         return (0);
     }
-  //  std::cout << _name << " port:" << std::to_string(_my_addr.sin_port) << " socket= " << std::to_string(_sockfd) << std::endl;
     if (bind(_sockfd, (struct sockaddr *)&_my_addr, sizeof _my_addr)<0)
     {
         perror("In bind");
         return(0);
     }
-    int yes=1;
-
-    // lose the pesky "Address already in use" error message
+    int yes = 1;
     if (setsockopt(_sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) {
         perror("setsockopt");
         return(0);
@@ -39,36 +36,7 @@ int Server::start(void)
     {
         perror("In listen");
         exit(EXIT_FAILURE);
-    }/*
-    int accept_fd = 0;
-    //struct sockaddr_in	client_addr;
-    int addrlen = sizeof(_my_addr);
-    //bzero(&client_addr, sizeof(client_addr));
-
-    while(1)
-    {
-  
-        if ((accept_fd = accept(_sockfd, (struct sockaddr *)&_my_addr, (socklen_t*)&addrlen)) == -1)
-        {
-            perror("accept");
-            return (0);
-        }
-        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((accept_fd = accept(_sockfd, (struct sockaddr *)&_my_addr, (socklen_t*)&addrlen))<0)
-        {
-            perror("In accept");
-            exit(EXIT_FAILURE);
-        }
-        
-        char buffer[30000] = {0};
-        read( accept_fd , buffer, 30000);
-        printf("%s\n",buffer );
-    // send(new_socket, pFile, pFile.tellg(), 0); //escribir con send
-    // write(new_socket , hello , strlen(hello));
-        write(accept_fd , "hola", 4);
-        printf("------------------Hello message sent-------------------\n");
-        close(accept_fd);
-    }*/
+    }
     return (1);
 }
 
@@ -92,8 +60,7 @@ int Server::acceptNewClient(void)
         char buffer[30000] = {0};
         read( accept_fd , buffer, 30000);
         printf("%s\n",buffer );
-    // send(new_socket, pFile, pFile.tellg(), 0); //escribir con send
-    // write(new_socket , hello , strlen(hello));
+    // send(new_socket, "hola", 4, 0); //escribir con send
         write(accept_fd , "hola", 4);
         printf("------------------Hello message sent-------------------\n");
         close(accept_fd);
